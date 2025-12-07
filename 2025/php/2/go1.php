b274d46278277657b06c86c52222e9ba22b520aa
Nicola Onofri <nicola.onofri@gmail.com>
2025-12-07 17:03:02 +0100

    First part 2nd day solved in PHP

--- 2025/php/2/go1.php
+++ 2025/php/2/go1.php
@@ -2,47 +2,62 @@
 
 $x = explode(',', file('input')[0]);
 
-$r = 0;
+$res = 0;
 
 foreach ($x as $n) {
     $p = explode('-', $n);
     $l = $p[0];
     $r = $p[1];
+
+    $res += solve($l, $r);
 }
 
-function start($input, $max)
+print($res . PHP_EOL);
+
+function solve($lower, $upper): int
 {
-    $l = strlen($input);
-
-    if ($l % 2 !== 0) {
-        $input = str_pad(
-          substr($input, 0, 1),
-          $l + 1,
-          '0'
-        );
+    // Ensure lower bound has even length by padding if necessary
+    if (strlen($lower) % 2 !== 0) {
+        $lower = '1'.str_repeat('0', strlen($lower));
     }
 
-    $x = strlen($input) / 2;
+    $half = strlen($lower) / 2;
 
-    $l = substr($input, 0, $x);
+    $left = substr($lower, 0, $half);
+    $right = substr($lower, $half, $half * 2);
 
-    $r = substr($input, $x, $x * 2);
-
-    if ($l === $r && (int) $input < (int) $max) {
-        return $input;
-    }
-
-    if ((int) $l < (int) $r) {
-        $f = (int) substr($l, $x - 1, 1) + 1;
-
-        $w = substr($l, 0, $x - 1).$f;
-
-        $r = $w.$w;
+    // Find the first symmetric candidate >= lower
+    if ($left === $right && (int) $lower < (int) $upper) {
+        $start = $lower;
+    }  else if ((int) $left < (int) $right) {
+        // Increment left half to get next symmetric number
+        $f = (int) substr($left, $half - 1, 1) + 1;
+        $w = substr($left, 0, $half - 1).$f;
+        $start = $w.$w;
     } else {
-        $r = $l.$l;
+        $start = $left.$left;
     }
 
-    return $r < $max ? $r : null;
-}
+    // If start is above upper bound, no valid symmetric numbers
+    if($start > $upper) {
+        return 0;
+    }
 
-print_r(start('12', '23'));
+    $sum = 0;
+
+    // Loop over symmetric candidates until exceeding upper bound
+    $left = substr($start, 0, strlen($start) / 2);
+
+    while(true) {
+        $candidate = $left . $left;
+
+        if((int) $candidate <= (int) $upper) {
+            $sum += (int) $candidate;
+            $left = $left + 1; // Move to next symmetric number
+        } else {
+            break;
+        }
+    }
+
+    return $sum;
+}
